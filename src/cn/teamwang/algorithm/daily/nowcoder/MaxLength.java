718344f434d88188e4259918876f548fa74ae9d2
jacky <jacky.teamwang@qq.com>
2021-05-10 17:52:22 +0800

    add daily exercises

--- src/cn/teamwang/algorithm/daily/nowcoder/MaxLength.java
+++ src/cn/teamwang/algorithm/daily/nowcoder/MaxLength.java
@@ -1,9 +1,6 @@
 package cn.teamwang.algorithm.daily.nowcoder;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 给定一个数组arr，返回arr的最长无的重复子串的长度(无重复指的是所有数字都不相同)。
@@ -13,33 +10,34 @@ import java.util.Map;
  */
 public class MaxLength {
     public static void main(String[] args) {
-//        int[] nums1 = {2, 3, 4, 5};
+        int[] nums1 = {2, 3, 4, 5};
         int[] nums2 = {2, 2, 3, 4, 3};
-//        System.out.println(maxLength(nums1));
+        System.out.println(maxLength(nums1));
         System.out.println(maxLength(nums2));
     }
 
     /**
-     * 先不考虑动态规划/双指针/滑动窗口，用自己的方法做出来先。
+     * 滑动窗口
      */
     public static int maxLength(int[] arr) {
-        int max = 1;
-        int len = 1;
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            list.add(arr[i]);
-            for (int j = i + 1; j < arr.length - 1; j++) {
-                if (list.contains(arr[j])) {
-                    list.clear();
-                } else {
-                    list.add(arr[j]);
-                    len++;
-                }
-                max = Math.max(len, max);
-            }
+        if (arr.length == 1) {
+            return 1;
         }
+        int max = 0, l = 0, r = -1;
+        // 用来统计每个数字(或字符)出现的次数，可以用map
+        // 用来统计每个数字(或字符)是否出现过，用set
+        Set<Integer> freq = new HashSet<>();
 
+        while (l < arr.length - 1 && r < arr.length - 1) {
+            if (!freq.contains(arr[r+1])) {
+                r++;
+                freq.add(arr[r]);
+            } else {
+                freq.remove(arr[l]);
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+        }
         return max;
     }
 }
