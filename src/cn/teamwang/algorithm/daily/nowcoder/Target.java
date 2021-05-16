fcd72013fe0d668ec7363188075704c34dca3b85
jacky <jacky.teamwang@qq.com>
2021-05-16 16:30:09 +0800

    optimized for brute force search

--- src/cn/teamwang/algorithm/daily/nowcoder/Target.java
+++ src/cn/teamwang/algorithm/daily/nowcoder/Target.java
@@ -1,6 +1,8 @@
 package cn.teamwang.algorithm.daily.nowcoder;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 给出一个整数数组，请在数组中找出两个加起来等于目标值的数，
@@ -9,7 +11,7 @@ import java.util.Arrays;
  * 例如：
  * 给出的数组为 {20, 70, 110, 150},目标值为90
  * 输出 index1=1, index2=2
- *
+ * <p>
  * https://www.nowcoder.com/practice/20ef0972485e41019e39543e8e895b7f?tpId=188&tqId=38285&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2Fjob-code-high-week%2Fquestion-ranking&tab=answerKey
  *
  * @author <a href="mailto:clorisforcoding@gmail.com">Jacky Fang</a>
@@ -22,8 +24,7 @@ public class Target {
     }
 
     /**
-     * O(N^2)
-     * 先用一般方法，再考虑优化(排序+二分法)
+     * 穷举所有情况：O(N^2)
      */
     public static int[] twoSum1(int[] numbers, int target) {
         // write code here
@@ -42,10 +43,48 @@ public class Target {
     }
 
     /**
-     * ERROR: 索引乱序了。
-     * O(NlogN)
+     * O(n)
+     * 因为要查找的目标值也在数组内，所以可以通过哈希表来保存、获取目标值，而不是二次遍历数组。
+     * 总结：穷举数组+答案唯一时，可考虑哈希表。
      */
     public static int[] twoSum2(int[] numbers, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            // 这里用num作为key是因为可以直接取出val。若使用索引作为key，则需要遍历才能通过val获取key。
+            // key 重复也没关系，因为答案是唯一的。
+            map.put(numbers[i], i);
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            int num = target - numbers[i];
+            if (map.containsKey(num) && i != map.get(num)) {
+                res[0] = i + 1;
+                res[1] = map.get(num) + 1;
+                // 注意这里的 break
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static int[] twoSum2_optimized(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    /**
+     * ERROR: 排序后索引乱序了。
+     * O(NlogN)
+     */
+    public static int[] twoSum3(int[] numbers, int target) {
         // write code here
         int[] res = new int[2];
         Arrays.sort(numbers);
--- src/doc/doc.md
+++ src/doc/doc.md
@@ -2,3 +2,23 @@
 队列：先进先出
 栈：先进后出
 链表 & 数组：链表是链式存储结构，数组是顺序存储结构。数组**查询修改**快(通过下标查找O(1)，无需遍历O(N))，链表**插入删除**快。
+
+穷举数组的优化方法：考虑哈希表
+```java
+    /**
+     * O(n)
+     * 因为要查找的目标值也在数组内，所以可以通过哈希表来保存、获取目标值，而不是二次遍历数组。
+     * 总结：穷举数组+答案唯一时，可考虑哈希表。
+     */
+    public static int[] twoSum2_optimized(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+```
