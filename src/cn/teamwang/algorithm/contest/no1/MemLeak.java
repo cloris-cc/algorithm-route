f61d7bcc3b6a9987bf16fcaccd4829c9c698da16
jacky <jacky.teamwang@qq.com>
2021-05-16 16:27:33 +0800

    optimized for leetcode contest No.1

--- src/cn/teamwang/algorithm/contest/no1/MemLeak.java
+++ src/cn/teamwang/algorithm/contest/no1/MemLeak.java
@@ -11,17 +11,18 @@ public class MemLeak {
         System.out.println(Arrays.toString(memLeak(8, 11)));
     }
 
+    /**
+     * 时间复杂度：O(n)
+     */
     public static int[] memLeak(int memory1, int memory2) {
         int[] res = new int[3];
         int i = 1;
 
         while (i <= Math.max(memory1, memory2)) {
-            if (memory1 > memory2) {
-                memory1 = memory1 - i;
-            } else if (memory1 < memory2) {
-                memory2 = memory2 - i;
+            if (memory1 < memory2) {
+                memory2 -= i;
             } else {
-                memory1 = memory1 - i;
+                memory1 -= i;
             }
             i++;
         }
--- src/cn/teamwang/algorithm/contest/no1/SortSentence.java
+++ src/cn/teamwang/algorithm/contest/no1/SortSentence.java
@@ -8,29 +8,43 @@ import java.util.Comparator;
  */
 public class SortSentence {
     public static void main(String[] args) {
-        System.out.println(sortSentence("is2 sentence4 This1 a3"));
+        System.out.println(sortSentence1("is2 sentence4 This1 a3"));
+        System.out.println(sortSentence2("is2 sentence4 This1 a3"));
     }
 
-    public static String sortSentence(String s) {
+    /**
+     * 时间复杂度：O(nlogn+n)=O(nlogn)
+     */
+    public static String sortSentence1(String s) {
         StringBuilder res = new StringBuilder();
         String[] strs = s.split(" ");
 
-        Arrays.sort(strs, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                int n1 = o1.charAt(o1.length() - 1);
-                int n2 = o2.charAt(o2.length() - 1);
-                return Integer.compare(n1, n2);
-            }
+        Arrays.sort(strs, (o1, o2) -> {
+            int n1 = o1.charAt(o1.length() - 1);
+            int n2 = o2.charAt(o2.length() - 1);
+            return Integer.compare(n1, n2);
         });
-//        for (int i = 0; i < strs.length; i++) {
-//            // 获取数字位，并根据数字排序strs
-//            int num = strs[i].charAt(strs[i].length() - 1);
-//
-//        }
-        for (String str : strs) { // is2
-            res.append(str.substring(0, str.length() - 1)).append(" ");
+        // 拼接排序后的单词
+        for (String str : strs) {
+            res.append(str, 0, str.length() - 1).append(" ");
         }
+        // 去末尾空格
         return res.toString().trim();
+    }
+
+    /**
+     * 时间复杂度O(n)
+     */
+    public static String sortSentence2(String s) {
+        String[] strs = s.split(" ");
+        String[] res = new String[strs.length];
+
+        for (String str : strs) {
+            // 把数字字符('0'~'9')当做下标
+            int num = str.charAt(str.length() - 1) - '0' - 1;
+            // 保存单词
+            res[num] = str.substring(0, str.length() - 1);
+        }
+        return String.join(" ", res);
     }
 }
