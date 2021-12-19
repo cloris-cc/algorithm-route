cc6bd049a5aaa15c54a40f7a3225a3c4d5ec0466
jacky <jacky.teamwang@qq.com>
2021-12-20 01:49:31 +0800

    update doc

--- src/cn/teamwang/algorithm/array/doc.md
+++ src/cn/teamwang/algorithm/array/doc.md
@@ -1,4 +1,5 @@
 数组的工具类：Arrays
+
 List集合的工具类：Collections
 
 二维数组排序：
@@ -6,10 +7,10 @@ List集合的工具类：Collections
 int [][]a = new int [5][2];
 
 Arrays.sort(a, new Comparator<int[]>() {
-@Override
-public int compare(int[] o1, int[] o2) {
-if (o1[0]==o2[0]) return o1[1]-o2[1];
-return o1[0]-o2[0];
-}
+    @Override
+    public int compare(int[] o1, int[] o2) {
+        if (o1[0]==o2[0]) return o1[1]-o2[1];
+        return o1[0]-o2[0];
+    }
 });
 ```
--- src/cn/teamwang/algorithm/sort/BubbleSort.java
+++ src/cn/teamwang/algorithm/sort/BubbleSort.java
@@ -3,7 +3,7 @@ package cn.teamwang.algorithm.sort;
 import java.util.Arrays;
 
 /**
- * 冒泡排序的概念：较小的数慢慢冒上来。
+ * 冒泡排序的概念：较大的数慢慢冒上来。
  * 时间复杂度：平均/最坏O(n^2) 最好O(n)
  *
  * @author <a href="mailto:clorisforcoding@gmail.com">Jacky Fang</a>
@@ -12,9 +12,9 @@ public class BubbleSort {
     public static void main(String[] args) {
         /*
          * 排序过程：
-         * i=0,第一趟：[1,5] [3,5] [4,5] [2,5] to {1,3,4,2,5}
-         * i=1,第二趟：[1,3] [3,4] [2,4] to {1,3,2,4,5}
-         * i=2,第三趟：[1,3] [2,3] [3,4] to {1,2,3,4,5}
+         * i=0,第一趟：[1,5] [3,5] [4,5] [2,5] to {1,3,4,2,5}，冒上来了第一(最)大值 5
+         * i=1,第二趟：[1,3] [3,4] [2,4] to {1,3,2,4,5}，冒上来了第二大值 4
+         * i=2,第三趟：[1,3] [2,3] [3,4] to {1,2,3,4,5}，冒上来了第三大值 3
          * i=3,第四趟：...
          */
         int[] nums = {5,1,3,4,2};
